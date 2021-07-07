53422ff85db2b25d33b60671a420be4fce48d2b3
Natasja <86358047+NatasjaPadilla@users.noreply.github.com>
2021-07-07 17:24:43 +0800

    Update bblsrt.java

--- bblsrt.java
+++ bblsrt.java
@@ -17,41 +17,38 @@ public class bblsrt {
 	Random rnd = new Random();
         int[] nums = new int[10];
         int temp = 0;
-        boolean sorted; // added boolean to determine true or false
+        boolean sorted;
         
-        // this for loop will fill the array with numbers
         for(int x = 0; x < nums.length; x++)
         {
-            nums[x] = rnd.nextInt(50); // generates the numbers
-            System.out.print(nums[x] + "\t"); // prints the numbers
+            nums[x] = rnd.nextInt(50); //generates the numbers
+            System.out.print(nums[x] + "\t"); //prints the numbers
         }
-        System.out.println(); // just a line break
+        System.out.println();
         
-        // this is the sorting algorithm
-        for(int x = 0; x < nums.length; x++) // the first loop dictates the number of passes
+        //this is the sorting algorithm
+        for(int x = 0; x < nums.length; x++)
         {	
-        	sorted = true; // reset
-            for(int y = 0; y < nums.length - 1; y++) // this is the loop that dictates the sorting
+        	sorted = true; //reset
+            for(int y = 0; y < nums.length - 1; y++)
             {
-                // algorithm proper
                 if(nums[y] > nums[y+1])
                 {
                     temp = nums[y];
                     nums[y] = nums[y+1];
                     nums[y+1] = temp;
-                    sorted = false; // sets a new value
+                    sorted = false; //sets a new value
                 }
      
               
             }
             
-            // optional display step
             System.out.print("Pass #" + (x+1) + ": \t");
             for(int z = 0; z < nums.length; z++)
             {
                 System.out.print(nums[z] + "\t");
             }
-            System.out.println(); // just a line break
+            System.out.println();
             
             if(sorted)
             {
